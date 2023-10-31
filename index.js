47897bb284f7313b74d7ebdc71f82c98a5560c55
justice jennings <justice.l.jennings@gmail.com>
2023-10-31 16:54:09 -0400

    added function and started iteration process.

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+// create prompt for user to input flavors and log
 prompt(`Enter a comma separated list of your favorite Froyo flavors:`);
 console.log(`vanilla, vanilla, vanilla, strawberry, coffee, coffee`);
 
+const userInput = ([]) => {
+
+}
+
+//create an object containing different flavor options and log
 const flavors = {
     vanilla: 3,
     coffee: 2,
