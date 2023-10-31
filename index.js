2dd34c746148918de229abe4c4d93c68ffe5838c
justice jennings <justice.l.jennings@gmail.com>
2023-10-31 16:57:49 -0400

    tweaked function and content in js file.

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
 // create prompt for user to input flavors and log
-prompt(`Enter a comma separated list of your favorite Froyo flavors:`);
-console.log(`vanilla, vanilla, vanilla, strawberry, coffee, coffee`);
+ prompt(`Enter a comma separated list of your favorite Froyo flavors:`);
+ 
+// define constant for user input string and log
+const userInput = `vanilla,vanilla,vanilla,strawberry,coffee,coffee`;
+console.log(userInput);
 
-const userInput = ([]) => {
+// split user input into an array of strings and log
+const userInputArr = userInput.split(",");
+console.log(userInputArr);
 
-}
-
-//create an object containing different flavor options and log
-const flavors = {
+// //create an object containing different flavor options and log
+const flavorsOrdered = {
     vanilla: 3,
     coffee: 2,
     strawberry: 1,
 };
-console.log(Object.keys(flavors));
-console.log(Object.values(flavors));
+console.log(flavorsOrdered);
+// iterate through EACH flavor
+// for(i = 0; i < flavorsOrdered.length; i++) {
+//     console.log(flavorsOrdered[i])
+
+    flavorsOrdered.forEach((Object.keys) => 
+    console.log(Object.values));
+    
+
+
+// // log and observe flavors keys and corresponding values
+// // for accuracy. 
+// console.log(Object.keys(flavors));
+// console.log(Object.values(flavors));
